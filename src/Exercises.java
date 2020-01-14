8a3e7006d9edbb02b067dfccddf663cf05f1a02e
Thomas Finnerty <34747130+tfinnerty02@users.noreply.github.com>
2020-01-14 09:09:33 -0500

    beautified

--- src/Exercises.java
+++ src/Exercises.java
@@ -1,69 +1,69 @@
 public class Exercises {
 
-	public boolean commonEnd(int[] a, int[] b) {
-		if (a == null || a.length == 0 || b == null || b.length == 0) {
-			return false;
-		}
+ public boolean commonEnd(int[] a, int[] b) {
+  if (a == null || a.length == 0 || b == null || b.length == 0) {
+   return false;
+  }
 
-		// write your code here
-		if (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]) {
-			return true;
-		}
+  // write your code here
+  if (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]) {
+   return true;
+  }
 
-		return false;
-	}
+  return false;
+ }
 
-	public String[] endsMeet(String[] values, int n) {
-		// write your code here
+ public String[] endsMeet(String[] values, int n) {
+  // write your code here
 
-		return null;	// default return value to ensure compilation
-	}
+  return null; // default return value to ensure compilation
+ }
 
-	public int difference(int[] numbers) {
-		// write your code here
+ public int difference(int[] numbers) {
+  // write your code here
 
-		return -1;		// default return value to ensure compilation
-	}
+  return -1; // default return value to ensure compilation
+ }
 
-	public double biggest(double[] numbers) {
-		// write your code here
+ public double biggest(double[] numbers) {
+  // write your code here
 
-		return -1;		// default return value to ensure compilation
-	}
+  return -1; // default return value to ensure compilation
+ }
 
-	public String[] middle(String[] values) {
-		// write your code here
+ public String[] middle(String[] values) {
+  // write your code here
 
-		return null;	// default return value to ensure compilation
-	}
+  return null; // default return value to ensure compilation
+ }
 
-	public boolean increasing(int[] numbers) {
-		// write your code here
+ public boolean increasing(int[] numbers) {
+  // write your code here
 
-		return false;	// default return value to ensure compilation
-	}
+  return false; // default return value to ensure compilation
+ }
 
-	public boolean everywhere(int[] numbers, int x) {
-		// write your code here
+ public boolean everywhere(int[] numbers, int x) {
+  // write your code here
 
-		return false;	// default return value to ensure compilation
-	}
+  return false; // default return value to ensure compilation
+ }
 
-	public boolean consecutive(int[] numbers) {
-		// write your code here
+ public boolean consecutive(int[] numbers) {
+  // write your code here
 
-		return false;	// default return value to ensure compilation
-	}
+  return false; // default return value to ensure compilation
+ }
 
-	public boolean balance(int[] numbers) {
-		// write your code here
+ public boolean balance(int[] numbers) {
+  // write your code here
 
-		return false;	// default return value to ensure compilation
-	}
+  return false; // default return value to ensure compilation
+ }
 
-	public int clumps(String[] values) {
-		// write your code here
+ public int clumps(String[] values) {
+  // write your code here
 
-		return -1;		// default return value to ensure compilation
-	}
+  return -1; // default return value to ensure compilation
+ }
 }
