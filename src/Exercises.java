c679118048721fcf627326ca94b75d4170184402
Thomas Finnerty <34747130+tfinnerty02@users.noreply.github.com>
2020-01-14 15:47:52 -0500

    started exercise 5

--- src/Exercises.java
+++ src/Exercises.java
@@ -5,7 +5,7 @@ public class Exercises {
 	public static void main(String[] args) {
 //		System.out.println(endsMeet(String[] values = {"1", "2", "3", "4", "5", "6"}, 2));
 		System.out.println("p");
-		
+
 //		int[] h = {1, 2, 3, 4, 5, 6, 7, 8};
 	}
 
@@ -38,9 +38,9 @@ public class Exercises {
 //		System.arraycopy(lastN, 0, firstLastN, n, n * 2);
 //
 //		return firstLastN;
-	
+
 		return null;
-		
+
 	}
 
 	public int difference(int[] numbers) {
@@ -50,45 +50,63 @@ public class Exercises {
 		}
 		int min = 0;
 		int max = 0;
-		
+
 		if (numbers.length > 0) {
-		    min = numbers[0];
-		    
-		    for (int number : numbers) {
-		        if (number < min) {
-		            min = number;
-		        }
-		    }
+			min = numbers[0];
+
+			for (int number : numbers) {
+				if (number < min) {
+					min = number;
+				}
+			}
 		}
-		
+
 		if (numbers.length > 0) {
-		    max = numbers[0];
-		    
-		    for (int number : numbers) {
-		        if (number > max) {
-		            max = number;
-		        }
-		    }
+			max = numbers[0];
+
+			for (int number : numbers) {
+				if (number > max) {
+					max = number;
+				}
+			}
 		}
-		
+
 		return max - min;
 	}
 
 	public double biggest(double[] numbers) {
-		// write your code here
+		if (numbers == null || numbers.length < 3 || numbers.length % 2 == 0) {
+			return -1;
+		}
+
+		double max;
 		double first = numbers[0];
 		double middle = numbers[numbers.length / 2];
 		double last = numbers[numbers.length - 1];
-		
-		// compare the three and return here
 
-		return -1; // default return value to ensure compilation
+		double[] three = { first, middle, last };
+
+		max = three[0];
+		for (int i = 0; i < three.length; i++) {
+			if (three[i] > max) {
+				max = three[i];
+			}
+		}
+		return max;
 	}
 
 	public String[] middle(String[] values) {
-		// write your code here
+		if (values == null || Arrays.asList(values).contains(null) || values.length < 3 || values.length % 2 == 0) {
+			return null;
+		}
 
-		return null; // default return value to ensure compilation
+		int middle = values.length / 2;
+		int start = middle - 1;
+		int end = middle + 1;
+		String[] newArray = Arrays.copyOfRange(values, start, end);
+
+		return newArray;
+
 	}
 
 	public boolean increasing(int[] numbers) {
