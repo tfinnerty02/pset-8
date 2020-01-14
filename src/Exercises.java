1a4b847ba5f97f39dbc7a51dbf6bfc69edb9ec9b
Thomas Finnerty <34747130+tfinnerty02@users.noreply.github.com>
2020-01-14 09:07:29 -0500

    exercise 1

--- src/Exercises.java
+++ src/Exercises.java
@@ -4,63 +4,66 @@ public class Exercises {
 		if (a == null || a.length == 0 || b == null || b.length == 0) {
 			return false;
 		}
-		
+
 		// write your code here
-		
-		return false;	// default return value to ensure compilation
+		if (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]) {
+			return true;
+		}
+
+		return false;
 	}
-	
+
 	public String[] endsMeet(String[] values, int n) {
 		// write your code here
-		
+
 		return null;	// default return value to ensure compilation
 	}
-	
+
 	public int difference(int[] numbers) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
-	
+
 	public double biggest(double[] numbers) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
-	
+
 	public String[] middle(String[] values) {
 		// write your code here
-		
+
 		return null;	// default return value to ensure compilation
 	}
 
 	public boolean increasing(int[] numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean everywhere(int[] numbers, int x) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean consecutive(int[] numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean balance(int[] numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public int clumps(String[] values) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
 }
